61afd0738a27248da6204f43afe1b3c86887dbd8
Sol Johnston <soljohnston777@gmail.com>
2019-11-25 22:27:24 -0700

    Initial upload

---
+++ ReadMe.md
@@ -0,0 +1,64 @@
+# Google PHP API Client Examples
+A simple Sign In example using Google PHP API Client.
+
+
+Create a Folder in your htdocs or www folder:
+
+sign_in
+
+
+Copy all the files there:
+
+client_secrets.json
+composer.json
+composer.lock
+google.php
+
+
+
+## Steps
+
+1. Create a Google Developer account on [Google Developer Console](https://console.developers.google.com).
+
+2. Create an App on [Google Developer Console - Create Project ](https://console.developers.google.com/projectcreate).
+
+3. Create an OAuth Client ID on [Google Developer Console - Create Client ID ](https://console.developers.google.com/apis/credentials/oauthclient).
+
+3.1 Put in your redirect_uris as:
+
+    http://localhost/sign_in/google.php
+    https://localhost/sign_in/google.php
+
+
+3.2 Download this file when creating your Credential:
+
+    client_secrets.json
+
+
+4. Install Google PHP API Client through composer.
+go to the Go to your htdocs or www folder then to the folder:
+cd sign_in/
+
+```cmd
+composer install
+```
+
+5. Put in your App's CLIENT_ID and CLIENT_SECRET (and project_id) in the file if you didn't download it
+
+    client_secrets.json
+
+
+{"web":{"client_id":"client_id.apps.googleusercontent.com",
+    "project_id":"login-174700",
+    "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+    "token_uri":"https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+    "client_secret":"client_secret",
+    "redirect_uris":["http://localhost/sign_in/google.php"]}}
+
+
+6. Start your PHP server if it isn't already running.
+
+
+7. Access the web page going to:
+http://localhost/sign_in/google.php
---
+++ google.php
@@ -0,0 +1,94 @@
+<?php
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+$google_redirect_url = 'http://localhost/sign_in/google.php';
+//start session
+session_start();
+//include google api files
+include_once 'vendor/autoload.php';
+// New Google client
+$gClient = new Google_Client();
+$gClient->setApplicationName('ApplicationName');
+$gClient->setAuthConfigFile('client_secrets.json');
+$gClient->addScope(Google_Service_Oauth2::USERINFO_PROFILE);
+$gClient->addScope(Google_Service_Oauth2::USERINFO_EMAIL);
+// New Google Service
+$google_oauthV2 = new Google_Service_Oauth2($gClient);
+// LOGOUT?
+if (isset($_REQUEST['logout'])) 
+{
+	unset($_SESSION["auto"]);
+	unset($_SESSION['token']);
+	$gClient->revokeToken();
+	header('Location: ' . filter_var($google_redirect_url, FILTER_SANITIZE_URL)); //redirect user back to page
+}
+// GOOGLE CALLBACK?
+if (isset($_GET['code'])) 
+{
+	$gClient->authenticate($_GET['code']);
+	$_SESSION['token'] = $gClient->getAccessToken();
+    header('Location: ' . filter_var($google_redirect_url, FILTER_SANITIZE_URL));
+    return;
+}
+// PAGE RELOAD?
+if (isset($_SESSION['token'])) 
+{
+    $gClient->setAccessToken($_SESSION['token']);
+}
+// Autologin?
+if(isset($_GET["auto"]))
+{
+	$_SESSION['auto'] = $_GET["auto"];
+}
+// LOGGED IN?
+if ($gClient->getAccessToken()) // Sign in
+{
+	//For logged in user, get details from google using access token
+	try {
+		$user = $google_oauthV2->userinfo->get();
+		$user_id              = $user['id'];
+		$user_name            = filter_var($user['givenName'], FILTER_SANITIZE_SPECIAL_CHARS);
+		$email                = filter_var($user['email'], FILTER_SANITIZE_EMAIL);
+		$gender               = filter_var($user['gender'], FILTER_SANITIZE_SPECIAL_CHARS);
+		$profile_url          = filter_var($user['link'], FILTER_VALIDATE_URL);
+		$profile_image_url    = filter_var($user['picture'], FILTER_VALIDATE_URL);
+		$personMarkup         = "$email<div><img src='$profile_image_url?sz=50'></div>";
+		$_SESSION['token']    = $gClient->getAccessToken();
+		
+		// Show user
+		echo '<br /><a href="'.$profile_url.'" target="_blank"><img src="'.$profile_image_url.'?sz=100" /></a>';
+		echo '<br /><a class="logout" href="?logout=1">Logout</a>';
+		
+		$boolarray = Array(false => 'false', true => 'true');
+		echo '<p>Was automatically login? '.$boolarray[isset($_SESSION["auto"])].'</p>';
+
+		//list all user details
+		echo '<pre>'; 
+		print_r($user);
+		echo '</pre>';  
+	} catch (Exception $e) {
+		// The user revoke the permission for this App! Therefore reset session token	
+		unset($_SESSION["auto"]);
+		unset($_SESSION['token']);
+		header('Location: ' . filter_var($google_redirect_url, FILTER_SANITIZE_URL));
+	}
+}
+else // Sign up
+{
+    //For Guest user, get google login url
+    $authUrl = $gClient->createAuthUrl();
+	$auth_url = $gClient->createAuthUrl();
+	// Fast access or manual login button?
+	if(isset($_GET["auto"]))
+	{
+
+		header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
+	}
+	else
+	{
+		echo '<p>Login?</p>';
+		// echo '<a class="login" href="'.$authUrl.'"><img src="images/google-login-button.png" /></a>';
+		echo "<a href='$auth_url'>Login Through Google </a>";
+	}
+}
